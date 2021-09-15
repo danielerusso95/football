590eb8492b318a4166cbef4b0d0644079a3d9f73
LucaReasy <desalvialuca@yahoo.it>
2021-09-15 15:35:08 +0200

    dai

--- src/main/java/it/softwareinside/football/FootballApplication.java
+++ src/main/java/it/softwareinside/football/FootballApplication.java
@@ -4,8 +4,10 @@ import java.io.IOException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class FootballApplication {
 
 	public static void main(String[] args) throws IOException {
