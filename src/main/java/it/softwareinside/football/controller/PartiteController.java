31ffaf7d19622402e4d9f601dcc4ec8508138a67
danieleRusso <russo.daniele0@gmail.com>
2021-09-15 11:21:37 +0200

    aggiunta rotta all Partite

--- src/main/java/it/softwareinside/football/controller/PartiteController.java
+++ src/main/java/it/softwareinside/football/controller/PartiteController.java
@@ -1,14 +1,18 @@
 package it.softwareinside.football.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import it.softwareinside.football.model.Partite;
 import it.softwareinside.football.service.ServicePartite;
 
 @RestController
+@RequestMapping("/api")
 public class PartiteController {
 	
 	
@@ -17,9 +21,9 @@ public class PartiteController {
 	
 	
 	
-	@GetMapping("/api")
-	public void metodo() throws IOException {
-		service.takePartite();
+	@GetMapping("/all")
+	public List<Partite> metodo() throws IOException {
+		return service.takePartite();
 	}
 
 }
